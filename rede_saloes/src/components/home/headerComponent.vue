d77ff52091b6607bdad192d4e17fa52ffe0bf7ce
7raul <raulmigueleb7@gmail.com>
2023-12-10 19:10:02 +0100

    Inicio
    Inciando o projeto

---
+++ rede_saloes/src/components/home/headerComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="header-container">
+            <h2>Header container</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'headerComponent'
+    }
+</script>
---
+++ rede_saloes/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <headerComponent />
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+  import headerComponent from '@/components/home/headerComponent.vue'
+  export default {
+    name: 'HomeView',
+
+    components: {
+      headerComponent 
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
