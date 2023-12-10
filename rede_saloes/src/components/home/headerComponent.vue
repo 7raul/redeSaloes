2699f0c0f4dec2bd08f774f7393daf955ff41cdf
7raul <raulmigueleb7@gmail.com>
2023-12-10 19:58:22 +0100

    sass

--- rede_saloes/src/components/home/headerComponent.vue
+++ rede_saloes/src/components/home/headerComponent.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
         <div class="header-container">
-            <h2>Header container</h2>
+            <div class="heade-1">
+                <div id="logo">
+                    <h1>Rede<span class="red">@</span>Salões</h1>
+                </div>
+
+                <div id="login-logut">
+                    <router-link to="/">Entrar</router-link> 
+                    <router-link to="/">Minha Empresa</router-link>
+                </div>
+            </div>
         </div>
     </div>
 </template>
