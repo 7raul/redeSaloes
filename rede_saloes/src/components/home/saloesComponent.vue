0c5611cbbecde16f1962d55e90a8a19cdbfaf019
7raul <raulmigueleb7@gmail.com>
2023-12-19 22:37:59 +0100

    saloes
    start

--- rede_saloes/src/components/home/saloesComponent.vue
+++ rede_saloes/src/components/home/saloesComponent.vue
@@ -2,7 +2,32 @@
     <div>
         <div class="saloes-component">
             <div class="card-saloes">
-                <h2>Salões</h2>
+                <div class="publicite-img">
+                    <img :src="publiciteImg" :alt="publiciteAlt">
+                </div>
+                <div class="profile">
+                    <div class="profile-content">
+                        <img :src="profileImg" :alt="profileAlt">
+                        <router-link to="/">Falta Mais</router-link>
+                    </div>
+
+                    <div class="follow-icon">
+                        <i class="ri-add-line"></i>
+                    </div>
+                </div>
+                <div class="day-time-salao">
+                    <div class="days">
+                        <i class="ri-calendar-2-line"></i>
+                        <p>Segunda - Sabado</p>
+                    </div>
+
+                    <div class="time">
+                        <i class="ri-time-line"></i>
+                        <p>07h - 22h</p>
+                    </div>
+                </div>
+
+                <button>Reservar</button>
             </div>
         </div>
     </div>
@@ -10,6 +35,19 @@
 
 <script>
     export default {
-        name: 'saloesComponent'
+        name: 'saloesComponent',
+
+        data() {
+            return {
+               publiciteImg: require("@/assets/salao.jpg"),
+               publiciteAlt: 'Imagem publicitária',
+               profileImg: require("@/assets/perfil.jpg"),
+               profileAlt: 'Imagem de perfil'
+            }
+        }
     }
 </script>
+
+<style scoped>
+    @import '@/styles/home/saloesComponent.sass'
+</style>
