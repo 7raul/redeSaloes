7ee58ba20f3641933ad60f9a39ca98016718c0f4
7raul <raulmigueleb7@gmail.com>
2023-12-19 23:56:39 +0100

    Saloes

--- rede_saloes/src/components/home/saloesComponent.vue
+++ rede_saloes/src/components/home/saloesComponent.vue
@@ -29,6 +29,64 @@
 
                 <button>Reservar</button>
             </div>
+
+            <div class="card-saloes">
+                <div class="publicite-img">
+                    <img :src="publiciteImg" :alt="publiciteAlt">
+                </div>
+                <div class="profile">
+                    <div class="profile-content">
+                        <img :src="profileImg" :alt="profileAlt">
+                        <router-link to="/">Falta Mais</router-link>
+                    </div>
+
+                    <div class="follow-icon">
+                        <i class="ri-add-line"></i>
+                    </div>
+                </div>
+                <div class="day-time-salao">
+                    <div class="days">
+                        <i class="ri-calendar-2-line"></i>
+                        <p>Segunda - Sabado</p>
+                    </div>
+
+                    <div class="time">
+                        <i class="ri-time-line"></i>
+                        <p>07h - 22h</p>
+                    </div>
+                </div>
+
+                <button>Reservar</button>
+            </div>
+
+            <div class="card-saloes">
+                <div class="publicite-img">
+                    <img :src="publiciteImg" :alt="publiciteAlt">
+                </div>
+                <div class="profile">
+                    <div class="profile-content">
+                        <img :src="profileImg" :alt="profileAlt">
+                        <router-link to="/">Falta Mais</router-link>
+                    </div>
+
+                    <div class="follow-icon">
+                        <i class="ri-add-line"></i>
+                    </div>
+                </div>
+                <div class="day-time-salao">
+                    <div class="days">
+                        <i class="ri-calendar-2-line"></i>
+                        <p>Segunda - Sabado</p>
+                    </div>
+
+                    <div class="time">
+                        <i class="ri-time-line"></i>
+                        <p>07h - 22h</p>
+                    </div>
+                </div>
+
+                <button>Reservar</button>
+            </div>
         </div>
     </div>
 </template>
