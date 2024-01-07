9674887bf8599c35d590113bede2e0fccdb81c0d
7raul <raulmigueleb7@gmail.com>
2024-01-07 19:51:48 +0100

    feedback continue

--- rede_saloes/src/components/home/feedbackComponent.vue
+++ rede_saloes/src/components/home/feedbackComponent.vue
@@ -3,9 +3,23 @@
         <div class="feedback-container">
             <h1>O que os <span class="red">clientes dizem?</span></h1>
 
-            <div class="public">
-                <textarea placeholder="Escreva aqui..."></textarea>
-                <button>Comentar</button>
+            <div class="input-feedbacks">
+                <div class="feedback-area">
+                    <textarea placeholder="Escreva aqui..."></textarea>
+                    <button>Comentar</button>
+                </div>
+
+                <div class="feedbacks-area">
+                    <div class="feedback">
+                        <div class="feedback-profile">
+                            <img :src="profileImg" :alt="profileAlt">
+                        </div>
+
+                        <div class="feedback-content">
+
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
