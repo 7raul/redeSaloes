db8a2095017d51ef681832c2f8d12495cbabf15a
7raul <raulmigueleb7@gmail.com>
2024-01-21 13:37:24 +0100

    homen, mackup done

--- rede_saloes/src/views/HomenView.vue
+++ rede_saloes/src/views/HomenView.vue
@@ -2,6 +2,7 @@
     <div>
         <headerComponent />
         <bannerHomen />
+        <searchHomen />
         <footerComponent />
     </div>
 </template>
@@ -9,6 +10,7 @@
 <script>
     import headerComponent from '@/components/home/headerComponent.vue'
     import bannerHomen from '@/components/homen/bannerHomen.vue'
+    import searchHomen from '@/components/homen/searchHomen.vue'
     import footerComponent from '@/components/home/footerComponent.vue'
     export default {
         name: 'HomenView',
@@ -16,6 +18,7 @@
         components: {
             headerComponent,
             bannerHomen,
+            searchHomen,
             footerComponent
         }
     }
